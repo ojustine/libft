f3c1a0c3b154e3fbfa033e235fd738fe30a71d16
ojustine <ojustine@student.21-school.ru>
2020-08-12 19:48:59 +0300

    [done v0.1]

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -12,78 +12,156 @@
 
 #include "libft.h"
 
-static size_t	words_count(char const *s, char c)
+//static size_t	words_count(char const *s, char c)
+//{
+//	size_t	words;
+//
+//	words = 0;
+//	if (!s || *s == '\0')
+//		return (0);
+//	while (*s)
+//	{
+//		if (*s != c && (*(s + 1) == c || *(s + 1) == '\0'))
+//			words++;
+//		s++;
+//	}
+//	return (words);
+//}
+//
+//static size_t	next_len(char const *s, char c)
+//{
+//	size_t	len;
+//
+//	len = 0;
+//	if (!s)
+//		return (0);
+//	while (*s)
+//	{
+//		if (*s == c)
+//			s++;
+//		if (*s != c && (*(s + 1) == c || *(s + 1) == '\0'))
+//		{
+//			len++;
+//			break ;
+//		}
+//		if (*s != c)
+//			len++;
+//		s++;
+//	}
+//	return (len);
+//}
+//
+//static char		**free_tab(char **split)
+//{
+//	while (*split)
+//	{
+//		free(*split);
+//		split++;
+//	}
+//	free(split);
+//	return (NULL);
+//}
+//
+//char			**ft_strsplit(char const *s, char c)
+//{
+//	char	**split;
+//	char	**ret;
+//	size_t	len;
+//
+//	if (!s || !(split = malloc(sizeof(char*) * words_count(s, c) + 1)))
+//		return (NULL);
+//	ret = split;
+//	while (*s)
+//	{
+//		if (*s != c && *s != '\0')
+//		{
+//			len = next_len(s, c);
+//			if ((*split = ft_strnew(len)) == NULL)
+//				return (free_tab(ret));
+//			ft_memcpy(*split, s, len);
+//			s += (len - 1);
+//			split++;
+//		}
+//		s++;
+//	}
+//	*split = NULL;
+//	return (ret);
+//}
+
+static size_t	ft_count_words(char const *s, char c)
 {
-	size_t	words;
+	size_t words;
 
 	words = 0;
-	if (!s || *s == '\0')
-		return (0);
 	while (*s)
 	{
-		if (*s != c && (*(s + 1) == c || *(s + 1) == '\0'))
+		while (*s == c)
+			s++;
+		if (*s)
+		{
 			words++;
-		s++;
+			while (*s && *s != c)
+				s++;
+		}
 	}
 	return (words);
 }
 
-static size_t	next_len(char const *s, char c)
+static char		*ft_get_word(char *word, char c)
 {
-	size_t	len;
+	char *start;
 
-	len = 0;
-	if (!s)
-		return (0);
-	while (*s)
-	{
-		if (*s == c)
-			s++;
-		if (*s != c && (*(s + 1) == c || *(s + 1) == '\0'))
-		{
-			len++;
-			break ;
-		}
-		if (*s != c)
-			len++;
-		s++;
-	}
-	return (len);
+	start = word;
+	while (*word && *word != c)
+		word++;
+	*word = '\0';
+	return (ft_strdup(start));
 }
 
-static char		**free_tab(char **split)
+static void		ft_free_words(char **words, size_t i)
 {
-	while (*split)
+	while (i--)
+		ft_strdel(&(words[i]));
+	free(*words);
+}
+
+static char		**ft_get_words(char *s, char c, size_t words_count)
+{
+	char	**words;
+	char	*word;
+	size_t	i;
+
+	i = 0;
+	if ((words = (char **)ft_memalloc(sizeof(char *) * (words_count + 1))))
 	{
-		free(*split);
-		split++;
+		while (i < words_count)
+		{
+			while (*s == c)
+				s++;
+			if (*s)
+			{
+				if (!(word = ft_get_word(s, c)))
+				{
+					ft_free_words(words, i);
+					return (NULL);
+				}
+				words[i++] = word;
+				s += (ft_strlen(word) + 1);
+			}
+		}
+		words[i] = NULL;
 	}
-	free(split);
-	return (NULL);
+	return (words);
 }
 
 char			**ft_strsplit(char const *s, char c)
 {
-	char	**split;
-	char	**ret;
-	size_t	len;
+	char	**words;
+	char	*line;
 
-	if (!s || !(split = malloc(sizeof(char*) * words_count(s, c) + 1)))
+	if (!s || !(line = ft_strdup((char *)s)))
 		return (NULL);
-	ret = split;
-	while (*s)
-	{
-		if (*s != c && *s != '\0')
-		{
-			len = next_len(s, c);
-			if ((*split = ft_strnew(len)) == NULL)
-				return (free_tab(ret));
-			ft_memcpy(*split, s, len);
-			s += (len - 1);
-			split++;
-		}
-		s++;
-	}
-	*split = NULL;
-	return (ret);
+	words = ft_get_words(line, c, ft_count_words(line, c));
+	free(line);
+	return (words);
 }
